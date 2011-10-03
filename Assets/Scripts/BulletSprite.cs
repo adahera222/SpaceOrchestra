902fc49049b4eb227ba72c5267e6a9d2881c82c0
unknown <Nicolas@.(none)>
2011-10-04 01:59:46 +0200

    -Remplacement du suppositoire de bullet par un billboard +zoli et -poly -Bidouillages sonores TODO: Collisions des bullets par raycasting

---
+++ Assets/Scripts/BulletSprite.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+
+public class BulletSprite : MonoBehaviour {
+	
+	public Camera current;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		// Position de la camera dans l'espace du sprite
+		Vector3 SpriteToCam = transform.InverseTransformPoint(Camera.mainCamera.transform.position);
+		Vector3 previous_right = transform.right;
+		// Projection du vecteur pour obtenir forward
+		SpriteToCam.x = 0.0f;
+		
+		transform.LookAt(transform.TransformPoint(SpriteToCam), transform.up);
+		
+	}
+}
+ 
